b78e23f08709db5e0ec7dcf9027273dbe2599f03
LISH0038 <melodyli710@gmail.com>
2019-09-14 13:15:25 +0800

    Refactor css files, add attendance list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./css/App.css";
 import Nav from "./components/Nav";
 import MainScreen from "./components/MainScreen";
 import Home from "./components/Home";
