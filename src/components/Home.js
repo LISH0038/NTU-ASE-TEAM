93cb19eb6dbdf672f9a0eab2117905727060f2fc
Akshaya Muthu <akshaya522@gmail.com>
2019-10-07 03:41:22 +0800

    Add course-index entry page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Button, FormGroup, Form, Label, Input } from "reactstrap"
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div>
-      <h1> Welcome Teaching Assistant  </h1>
-    </div>
+    <Form>
+      <h1 className="text-center pt-3 ">  Welcome Teaching Assistant  </h1>
+
+      <FormGroup className="loginForm mt-5 mb-3">
+        <Label className="mt-2 labelLogin" style={{ color: 'white', fontWeight: 'bold' }}> Course Index</Label>
+        <Input type="index" placeholder="index" className="mt -3" />
+        <Label className="mt-4" style={{ color: 'white', fontWeight: 'bold' }}> Tutorial/Lab Index</Label>
+        <Input type="index#" placeholder="index#" />
+      </FormGroup>
+
+      <Link to="/mainscreen">
+        <Button className="btn-lg btn-dark btn-block mt-5 btnLogin"> View Attendance</Button>
+      </Link>
+    </Form >
   );
 }
 
