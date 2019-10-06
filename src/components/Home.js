94873a120e6183c5cb594365f32e5a586fde04e8
Akshaya Muthu <akshaya522@gmail.com>
2019-10-07 03:52:45 +0800

    index page design changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,8 @@ function Home() {
       <h1 className="text-center pt-3 ">  Welcome Teaching Assistant  </h1>
 
       <FormGroup className="loginForm mt-5 mb-3">
-        <Label className="mt-2 labelLogin" style={{ color: 'white', fontWeight: 'bold' }}> Course Index</Label>
+        <h4 style={{ color: 'white' }}> Enter Course Index</h4>
+        <Label className="mt-5 labelLogin" style={{ color: 'white', fontWeight: 'bold' }}> Course Index</Label>
         <Input type="index" placeholder="index" className="mt -3" />
         <Label className="mt-4" style={{ color: 'white', fontWeight: 'bold' }}> Tutorial/Lab Index</Label>
         <Input type="index#" placeholder="index#" />
