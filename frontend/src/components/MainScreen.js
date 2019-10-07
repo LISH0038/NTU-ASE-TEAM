f73de009c266fb5175c105a622b0f4fc74706ae3
LISH0038 <melodyli710@gmail.com>
2019-10-07 11:49:19 +0800

    request example

--- frontend/src/components/MainScreen.js
+++ frontend/src/components/MainScreen.js
@@ -8,6 +8,14 @@ import "../css/MainScreen.css";
 
 
 function MainScreen() {
+  //API call reference
+  const request = require('request');
+  request('http://localhost:3000/index/10001', function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+  });
+  
   const expectedStudents = ["expectedStudent1", "expectedStudent2", "expectedStudent3"] // references to components
   const recognizedStudents = ["recognizedStudent1", "recognizedStudent2", "recognizedStudent3"]
 
