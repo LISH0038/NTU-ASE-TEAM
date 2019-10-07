1796c5a2226554cb33d0925d318dc14b71ed7d52
kcatstack <ngocthaicao@gmail.com>
2019-10-07 11:33:45 +0800

    enable cors

--- backend/apiservice/api/app.js
+++ backend/apiservice/api/app.js
@@ -10,6 +10,9 @@ var recordRouter = require('./routes/record');
 
 var app = express();
 
+var cors = require('cors');
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
